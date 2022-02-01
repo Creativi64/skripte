ff62554159aad9e4d6045aab08e9d72718ca04af
Philipp <creativi.64.64@gmail.com>
2022-02-01 13:05:07 +0100

    ads

--- 2darr.sh
+++ 2darr.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-
 readarray -t testarray < CSV/schulaufgabe.csv
+
+if [ $1 -gt ${#testarray[@]} ]
+
+then
+
+echo "erster übergabeparameter zu groß"
+
+exit 0
+
+fi
+
+if [ $2>5 ]
+
+then
+
+echo "zweiter übergabeparameter zu groß"
+
+exit 0
+
+fi
 
 echo ${testarray[$1]} | cut -d ',' -f $2
