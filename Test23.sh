d12237c8df40421e26186c6a783febe2b0c72ade
Creativi64 <creativi.64.64@gmail.com>
2022-06-08 01:05:40 +0200

    asd

---
+++ Test23.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+mapfile -t testarray <ips.csv
+
+printf "%s\n" "${testarray[*]}"
+echo "---"
+printf "%s\n" "${testarray[@]}"
+echo "---"
+echo ${#testarray[*]}
+echo "---"
+echo ${#testarray[@]}
+echo "---"
+echo "${testarray[1]}"
+echo "---"
+echo ${#testarray[1]}
+echo "---"
+echo "${!testarray[*]}"
+echo "---"
+echo "${!testarray[@]}"
+echo "---"
+
+#hiujhjihjilhjk
